5d012511d8ca4ce9587d42c5406817d1080d8498
Maroua Ziouche <maroua.ziouche@etu.usthb.dz>
2024-12-02 22:22:31 +0100

    updates

--- test_lexer.py
+++ test_lexer.py
@@ -4,20 +4,12 @@ from compiler import test_lexer
 
 
 code = '''
-        %% Variable declarations
         int a;
-        float b = 4.2;
-        char c = 'X';
-        int arr[2];
-        const int MAX = 100;
-
-        %% Assignments and expressions
-        a = 10;
-        b = a + 3.5;
-
-        %% Read and write operations
-        READ(a);
-        WRITE(a, b, c);
+        char b[5];
+        float c = 3.14;
+        READ(c);
+        WRITE(c);
+        WRITE(b[2]);
 
         
 '''
