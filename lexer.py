b832238f442421c1eb8bd73bc6b060490e80dacc
rymaatb <rymaaitbraham@gamil.com>
2024-12-19 16:46:39 +0100

    separation du lexer du parser

---
+++ lexer.py
@@ -0,0 +1,113 @@
+import ply.lex as lex
+import ply.yacc as yacc
+import sys
+from tabulate import tabulate
+
+
+# List of tokens
+tokens = (
+    'VAR_GLOBAL', 'DECLARATION' ,  'CONST','INSTRUCTION',  # mots-clés
+    'ID', 'COMMENT','TRUE','FALSE',
+    'LBRACKET', 'RBRACKET',
+    'SEMICOLON', 'COMMA','LBRACE', 'RBRACE',
+    'NUMBER', 'PLUS', 'MINUS', 'MULTIPLY', 'DIVIDE', 'LPAREN', 'RPAREN',
+    'EQ', 'NEQ', 'LT', 'LTE', 'GT', 'GTE', 'AND', 'OR', 'NOT',
+    'EQUALS', 'INTEGER','BOOL', 'FLOAT', 'CHAR', 'INT_TYPE', 'FLOAT_TYPE', 'BOOL_TYPE', 'CHAR_TYPE',  'COLON',  
+    'IF', 'ELSE', 'FOR',  # Mots-clés du premier code
+    'READ', 'WRITE' # entrees et sorties
+
+    
+)
+
+# Reserved keywords
+reserved = {
+    'IF': 'IF',
+    'ELSE': 'ELSE',
+    'FOR': 'FOR',
+    'INTEGER': 'INT_TYPE',
+    'FLOAT': 'FLOAT_TYPE',
+    'bool': 'BOOL_TYPE',
+    'CHAR': 'CHAR_TYPE',
+    'true': 'BOOL',
+    'false': 'BOOL',
+    'CONST': 'CONST',
+    'READ': 'READ',
+    'WRITE': 'WRITE',
+}
+
+# Regular expressions
+# Expressions régulières pour les mots-clés
+t_VAR_GLOBAL = r'VAR_GLOBAL'
+t_DECLARATION = r'DECLARATION'
+t_INSTRUCTION = r'INSTRUCTION'
+
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_COMMA = r','
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_EQUALS = r'='
+t_EQ = r'=='
+t_NEQ = r'!='
+t_LT = r'<'
+t_LTE = r'<='
+t_GT = r'>'
+t_GTE = r'>='
+t_AND = r'&&'
+t_OR = r'\|\|'
+t_NOT = r'!'
+
+# regular expressions for read and write
+t_READ = r'READ'
+t_WRITE = r'WRITE'
+
+# Identifier and constants
+def t_ID(t):
+    r'[A-Za-z_][a-zA-Z0-9_]*'  # Permet les lettres, chiffres et underscores
+    t.type = reserved.get(t.value, 'ID')  # Vérifie si l'identifiant est un mot-clé
+    if t.value == 'true':
+        t.value = True
+    elif t.value == 'false':
+        t.value = False
+    return t
+
+
+def t_FLOAT(t):
+    r'[+-]?\d+\.\d+'
+    t.value = float(t.value)
+    
+    return t
+
+def t_INTEGER(t):
+    r'-?\d+'  # Cela permet de capturer les entiers avec un signe négatif optionnel
+    t.value = int(t.value)
+    return t
+
+
+def t_CHAR(t):
+    r"\'(.)\'" 
+    t.value = t.value[1]
+    return t
+
+
+
+# Ignore comments
+def t_COMMENT(t):
+    r'%%.*'
+    pass
+
+# Ignore spaces and tabs
+t_ignore = ' \t'
+
+# Handle errors
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}' at line {t.lineno}")
+    sys.exit(1)
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
